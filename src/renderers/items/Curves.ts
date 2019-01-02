7473e38c19f0495bf75e15bfc2f03f346e632e98
James Thompson <thisguy@jimmythompson.me>
2019-01-02 22:49:00 +0000

    Added controls for curves canvas ting

--- src/renderers/items/Curves.ts
+++ src/renderers/items/Curves.ts
@@ -1,17 +1,14 @@
 import Mixin, { Options } from '../Mixin';
 
 const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+const POINTER_RADIUS = 4;
+const POINTER_DUMMY = 2; // how mant pixels each side of the pointer, with it counting as clicked
 
 export default class Basic extends Mixin {
 
-  private startPointX: number;
-  private startPointY: number;
+  private points: any;
 
-  private endPointX: number;
-  private endPointY: number;
-
-  private controlPointX: number;
-  private controlPointY: number;
+  private selectedPoint: false | string;
 
   constructor(props: Options) {
     super(props);
@@ -19,62 +16,90 @@ export default class Basic extends Mixin {
     const width = this.getWidth();
     const height = this.getHeight();
 
-    this.startPointX = width / 2;
-    this.startPointY = height / 2;
-
-    this.endPointX = width / 2;
-    this.endPointY = 0;
-
-    this.controlPointX = 5;
-    this.controlPointY = height / 2;
-
-    this.canvasClick = this.canvasClick.bind(this);
-    // this.canvasPointerDown = this.canvasPointerDown.bind(this);
-
-    this.setToast(`Click anywhere to place the control point for the curve`);
-
-    canvas.addEventListener('click', this.canvasClick);
-    // canvas.addEventListener('pointerdown', this.canvasPointerDown);
-  }
-
-  // public canvasPointerDown(e: PointerEvent) {
-  //   const { pageX, pageY } = e;
-  //   console.log(pageX, pageY);
-  // }
-
-  private getPoints() {
-    return {
-      start: [this.startPointX, this.startPointY],
-      end: [this.endPointX, this.endPointY],
-      control: [this.controlPointX, this.controlPointY],
+    this.points = {
+      start: [ width / 2, height / 2 ],
+      end: [ width / 2, 0 ],
+      control: [ 5, height / 2 ],
     };
+
+    this.selectedPoint = false;
+
+    this.pointerDown = this.pointerDown.bind(this);
+    this.pointerMove = this.pointerMove.bind(this);
+    this.pointerUp = this.pointerUp.bind(this);
+
+    this.setToast(`Drag the points to change the curve`);
+
+    canvas.addEventListener('pointerdown', this.pointerDown);
+    canvas.addEventListener('pointermove', this.pointerMove);
+    canvas.addEventListener('pointerup', this.pointerUp);
+  }
+
+  public pointerMove(e: PointerEvent) {
+    const { pageX, pageY } = e;
+  
+    if (this.selectedPoint === false) {
+      const hoveredPoint = this.getHoveredPoint(pageX, pageY);
+      if (hoveredPoint === false) canvas.style.cursor = '';
+      else canvas.style.cursor = 'pointer';
+      return;
+    }
+
+    canvas.style.cursor = 'grab';
+    
+
+    this.points[this.selectedPoint][0] = pageX;
+    this.points[this.selectedPoint][1] = pageY;
+    this.queueRaf();
+  }
+
+  public pointerUp(e: PointerEvent) {
+    this.selectedPoint = false;
+  }
+
+  public getHoveredPoint(pointerX: number, pointerY: number) {
+
+    const possiblePoints = Object
+      .keys(this.points)
+      .reduce((selected: any[], key) => {
+        const [x, y] = this.points[key];
+        const pxFromXCenter = Math.abs(x - pointerX);
+        const pxFromYCenter = Math.abs(y - pointerY);
+        const dopeVariableName = POINTER_RADIUS * 2 + POINTER_DUMMY * 2;
+        if (pxFromXCenter <= dopeVariableName && pxFromYCenter <= dopeVariableName) {
+          selected.push({ key, x:pxFromXCenter, y:pxFromYCenter });
+        }
+        return selected;
+      }, []);
+
+      if (possiblePoints.length === 0) return false;
+      return possiblePoints.sort((a, b) => {
+        const { x:aX, y:aY } = a;
+        const { x:bX, y:bY } = b;
+
+        if (aX < bX && aY < bY) return -1;
+        if (aX > bX && aY > bY) return 1;
+
+        return aX + aY < bX + bY
+        ? -1
+        : 1
+      })[0].key;
+
   }
 
 
-
-  public canvasClick(e: MouseEvent) {
+  public pointerDown(e: PointerEvent) {
     const { pageX, pageY } = e;
+    const selectedPoint = this.getHoveredPoint(pageX, pageY);
 
-    const currentPoints = this.getPoints();
-
-    // TODO: add point detection??
-    // const pointToControl = Object
-    //   .keys(currentPoints)
-
-
-
-
-
-
-
-    // this.controlPointX = pageX;
-    // this.controlPointY = pageY;
-    // this.queueRaf();
+    this.selectedPoint = selectedPoint;
   }
 
   public beforeDestroy() {
-    canvas.removeEventListener('click', this.canvasClick);
-    // canvas.removeEventListener('pointermove', this.canvasPointerMove);
+    canvas.removeEventListener('pointerdown', this.pointerDown);
+    canvas.removeEventListener('pointermove', this.pointerMove);
+    canvas.removeEventListener('pointerup', this.pointerUp);
+    canvas.style.cursor = '';
   }
 
   public draw() {
@@ -86,23 +111,26 @@ export default class Basic extends Mixin {
 
     ctx.beginPath();
 
-    ctx.moveTo(this.startPointX, this.startPointY);
-    ctx.quadraticCurveTo(this.controlPointX, this.controlPointY, width / 2, 0);
+    ctx.moveTo(this.points.start[0], this.points.start[1]);
+    ctx.quadraticCurveTo(
+      this.points.control[0], 
+      this.points.control[1],
+      this.points.end[0],
+      this.points.end[1],
+    );
     ctx.stroke();
     ctx.closePath();
 
-    const points = [
-      [this.startPointX, this.startPointY],
-      [this.controlPointX, this.controlPointY],
-      [this.endPointX, this.endPointY],
-    ];
 
-    points.forEach(([x, y]) => {
-      ctx.beginPath();
-      ctx.arc(x, y, 4, 0, Math.PI * 2);
-      ctx.fill();
-      ctx.closePath();
-    });
+
+    Object.keys(this.points)
+      .map((key) => this.points[key])
+      .forEach(([x, y]) => {
+        ctx.beginPath();
+        ctx.arc(x, y, POINTER_RADIUS, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
+      });
 
     // ctx.beginPath();
     // ctx.arc(this.controlPointX, this.controlPointY, 4, 0, Math.PI * 2);
